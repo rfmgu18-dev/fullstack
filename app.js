6b68c7d523f19d189c3050596c1ea1e5eb2567e0
CristiandLara <cris19032006@gmail.com>
2026-07-10 14:54:37 -0400

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+( async() =>{
+
+    try {
+        const conection = await mongoose.connect(process.env.MONGO_URI_TEST);
+        console.log(conection);
+        
+        console.log("Conectado a MongoDB exitosamente");
+        
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+} )();
