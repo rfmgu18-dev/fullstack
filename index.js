375ed32906b70d50bfc17e18c30d51f3a77aab7b
leirbga <smortad.legedary@gmail.com>
2026-07-10 15:38:09 -0400

    prueba

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require('./app.js');
+const http = require('http');
+
+const server = http.createServer(app);
+
+server.listen(3003, () =>{
+    console.log('El servidor esta ejecutandose en el puerto 3000');
+})
